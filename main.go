5016d98372ff32be37536d429dd94892e88d3b11
Gustavo-Blois <gustavobloisrr@gmail.com>
2025-01-25 23:28:28 -0300

    versão final

--- main.go
+++ main.go
@@ -1,62 +1,152 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
 	"net/http"
-	"net/url"
-	"io"
 	"os"
-	"strings"
+	"strconv"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func Getauth() map[string]interface{} {
-        tokenURL := "http://oauth.battle.net/token"
-        clientID := os.Getenv("CLIENTID")
-        clientSECRET := os.Getenv("CLIENTSECRET")
+type Auction struct {
+	ID        int    `json:"id"`
+	Item      Item   `json:"item"`
+	Quantity  int    `json:"quantity"`
+	UnitPrice int    `json:"unit_price"`
+	TimeLeft  string `json:"time_left"`
+}
 
-	if clientID == "" || clientSECRET == "" {
-    		fmt.Println("Variáveis de ambiente CLIENTID ou CLIENTSECRET não estão definidas")
-    		return nil
+type Item struct {
+	ID int `json:"id"`
+}
+
+type AuctionsData struct {
+	Auctions []Auction `json:"auctions"`
+}
+
+var (
+	authToken   string    	
+  tokenExpiry time.Time
+)
+
+func getAuth() (string, time.Time) {
+	clientID := os.Getenv("CLIENTID")
+	clientSecret := os.Getenv("CLIENTSECRET")
+	postURL := "https://oauth.battle.net/token"
+
+	if clientID == "" || clientSecret == "" {
+		fmt.Println("CLIENTID ou CLIENTSECRET não configurados.")
+		os.Exit(1)
 	}
 
-        data := url.Values{}
-        data.Set("grant_type","client_credentials")
+	data := bytes.NewBufferString("grant_type=client_credentials")
+	request, err := http.NewRequest("POST", postURL, data)
+	if err != nil {
+		panic(err)
+	}
 
-        req, err := http.NewRequest("POST", tokenURL, strings.NewReader(data.Encode()))
-        if err != nil {
-                fmt.Println("Erro ao criar a requisição:",err)
-                return nil
-        }
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.SetBasicAuth(clientID, clientSecret)
 
-        req.SetBasicAuth(clientID,clientSECRET)
-        req.Header.Set("Content-Type","application/x-www-form-urlencoded")
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 
-        client := &http.Client{}
-        resp, err := client.Do(req)
-        if err != nil {
-                fmt.Println("Erro ao ler a resposta:",err)
-                return nil
-        }
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 
-		defer resp.Body.Close()
-		
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Erro ao ler a resposta:",err)
-		}
+	var respMap map[string]interface{}
+	json.Unmarshal(bodyResp, &respMap)
 
-        if resp.StatusCode == http.StatusOK {
-				var result map[string]interface{}
-                json.Unmarshal(body, &result)
-				return result
-        } else{
-            return nil
-        }
+	token := respMap["access_token"].(string)
+	expiresIn := respMap["expires_in"].(float64)
+	expiryTime := time.Now().Add(time.Duration(expiresIn) * time.Second)
+
+	return token, expiryTime
+}
+
+func ensureAuthToken() string {
+	if authToken == "" || time.Now().After(tokenExpiry) {
+		authToken, tokenExpiry = getAuth()
+		fmt.Println("Token renovado.")
+	}
+	return authToken
+}
+
+func sendRequest(authToken string) AuctionsData {
+	getURL := "https://us.api.blizzard.com/data/wow/auctions/commodities?namespace=dynamic-us&locale=en_US"
+	request, err := http.NewRequest("GET", getURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	authHeader := fmt.Sprintf("Bearer %s", authToken)
+	request.Header.Set("Authorization", authHeader)
+
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var data AuctionsData
+	json.Unmarshal(bodyResp, &data)
+	return data
 }
 
 func main() {
-	authTOK := Getauth()
-	fmt.Println("Token: ",authTOK)
-	return
+	botToken := os.Getenv("BOTTOK")
+	groupID := os.Getenv("GROUPID")
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		panic("Erro ao obter bot: " + err.Error())
+	}
+
+	groupIDInt, err := strconv.Atoi(groupID)
+	if err != nil {
+		panic("GROUPID deve ser um número inteiro.")
+	}
+
+	for {
+		authToken := ensureAuthToken()
+
+		auctionData := sendRequest(authToken)
+
+		minPrice := math.MaxInt
+		for _, auction := range auctionData.Auctions {
+			if auction.Item.ID == 210810 && auction.UnitPrice < minPrice {
+				minPrice = auction.UnitPrice
+			}
+		}
+
+		if minPrice > 910000 {
+			notification := fmt.Sprintf("O preço de venda da lança de Arathor 3 estrelas é %d", minPrice)
+			msg := tgbotapi.NewMessage(int64(groupIDInt), notification)
+			_, err = bot.Send(msg)
+			if err != nil {
+				fmt.Println("Erro no envio da mensagem:", err)
+			}
+		}
+
+		time.Sleep(time.Hour)
+	}
 }
+
